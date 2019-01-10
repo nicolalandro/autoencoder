7e27029739d880b5383498318a56079f4b741306
nicolalandro <nicolaxx94@live.it>
2019-01-10 14:28:05 +0100

    train work

--- train.sh
+++ train.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mkdir prepared_data
 python src/prepare-data.py input_data.txt prepared_data
+mkdir saved_model
 python src/train-autoencoder.py -i 1 -e 1 -b 50 saved_model prepared_data/vocabulary.txt prepared_data/train-data.npz prepared_data/valid-data.npz
 
